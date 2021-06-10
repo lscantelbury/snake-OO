39980696de3e3a898cab99f0b3e2b93b26a52d80
markus-gabryel <mgmds.snf20@uea.edu.br>
2021-06-10 17:22:11 -0400

    Add main menu implementation and its components

--- menu.py
+++ menu.py
@@ -1,41 +1,76 @@
 import pygame
-import time
 
-pygame.init()
 
-class Menu():
-    def __init__(self, x, y):
-         self.x = x
-         self.y = y
+class Image(pygame.sprite.Sprite):
+    def __init__(self, image, **rect_args):
+        super().__init__()
+        self.image = pygame.image.load(image) if type(image) is str else image
+        self.rect = self.image.get_rect(**rect_args)
 
-    def menuwindow(self):
-        pygame.display.set_mode((self.x, self.y))
-        pygame.display.set_caption(('Snake Menu'))
 
-class Start():
-    def __init__(self, x, y, font, color, text):
-        self.x = x
-        self.y = y
-        self.font = font
-        self.color = color
+class Text(Image):
+    def __init__(self, text, font, color=(255, 255, 255), **rect_args):
         self.text = text
-        
-    def drawstart(self):
-        starttext = self.font.render(self.text, True, self.color)
-        rect = starttext.get_rect()
-        Menu.self.blit(rect, (self.x, self.y))
-        Menu.pygame.display.flip()
+        image = font.render(text, False, color)
+        super().__init__(image, **rect_args)
 
-window = Menu(800, 600)
 
-buttonfont = pygame.font.SysFont('times new roman', 20)
-startbutton = Start(400, 300, buttonfont, (255,255,255), 'Start')
+class Button(Text):
+    def is_clicked(self):
+        return pygame.mouse.get_pressed()[0] and \
+            self.rect.collidepoint(pygame.mouse.get_pos())
 
-window.menuwindow()
-startbutton.drawstart()
 
-time.sleep(5)
+class MainMenu(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
 
-pygame.quit()
+        title_font = pygame.font.SysFont('times new roman', 90)
+        button_font = pygame.font.SysFont('times new roman', 50)
 
-quit()
+        self.bg = Image('snakebg.png')
+
+        self.title = Text('Snake', title_font, center=(400, 100))
+
+        self.start_button = Button("Start", button_font, center=(400, 250))
+        self.credits_button = Button("Credits", button_font, center=(400, 350))
+        self.quit_button = Button("Quit", button_font, center=(400, 450))
+
+        self.add(self.bg,
+                 self.title,
+                 self.start_button,
+                 self.credits_button,
+                 self.quit_button)
+
+
+# Testing...
+if __name__ == '__main__':
+    pygame.init()
+
+    menu = MainMenu()
+
+    screen = pygame.display.set_mode((800, 600))
+    clock = pygame.time.Clock()
+
+    done = False
+
+    while not done:
+        clock.tick(60)
+
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                done = True
+
+        if menu.start_button.is_clicked():
+            print('start')
+        elif menu.credits_button.is_clicked():
+            print('credits')
+        elif menu.quit_button.is_clicked():
+            print('quit')
+            done = True
+
+        menu.draw(screen)
+        pygame.display.flip()
+
+    pygame.quit()
+    exit()
